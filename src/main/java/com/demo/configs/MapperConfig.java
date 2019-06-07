4d24db951f6c06a86db5405e67c33b16105c831e
helloPei <hellopeiwen@163.com>
2019-06-07 21:14:06 +0800

    项目初始化

--- src/main/java/com/demo/configs/MapperConfig.java
+++ src/main/java/com/demo/configs/MapperConfig.java
@@ -12,25 +12,22 @@ import tk.mybatis.spring.mapper.MapperScannerConfigurer;
  * 通用mapper配置类
  */
 @Configuration
-public class MyBatisMapperConfig {
+public class MapperConfig {
 
 	/**
 	 * 使用通用Mapper之前需要初始化的一些信息 使用通用Mapper插件时请勿使用热加载,否则报错,插件作者后续应该会修复
 	 */
 	@Bean
 	public MapperScannerConfigurer mapperScannerConfigurer() {
-
 		MapperScannerConfigurer mapperScannerConfigurer = new MapperScannerConfigurer();
 		mapperScannerConfigurer.setSqlSessionFactoryBeanName("sqlSessionFactory");
 		mapperScannerConfigurer.setBasePackage("com.demo.mapper");// 普通mapper的位置
-
+		
 		Properties properties = new Properties();
 		properties.setProperty("mappers", BaseMapper.class.getName());// 通用mapper的全名
 		properties.setProperty("notEmpty", "false");
 		properties.setProperty("IDENTITY", "MYSQL");// 配置数据库方言
-
 		mapperScannerConfigurer.setProperties(properties);
-
 		return mapperScannerConfigurer;
 	}
 
